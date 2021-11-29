8e379afe387d145ae8ebb9735447560cb8a7543a
EdgarG4m3r <killzone551@ymail.com>
2021-11-29 15:18:17 +0800

    Update Followers.php

--- app/Models/Followers.php
+++ app/Models/Followers.php
@@ -4,12 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+
 
 class Followers extends Model
 {
     use HasFactory;
+
     protected $fillable = [
-        'user_id_1',
-        'user_id_2',
+        'user_id_1', 'user_id_2'
     ];
+
+    public function getCreatedAtAttribute()
+    {
+        if (!is_null($this->attributes['created_at']))
+        {
+            return Carbon::parse($this->attributes['created_at'])->format('Y-m-d H:i:s');
+        }
+    }
+
+    public function getUpdatedAtAttribute()
+    {
+        if (!is_null($this->attributes['updated_at']))
+        {
+            return Carbon::parse($this->attributes['updated_at'])->format('Y-m-d H:i:s');
+        }
+    }
 }
