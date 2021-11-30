39416b6c12af43a836cdb47b4327b14aba5bef99
EdgarG4m3r <killzone551@ymail.com>
2021-11-30 20:52:07 +0800

    Add followers & post routes

--- routes/api.php
+++ routes/api.php
@@ -25,4 +25,19 @@ Route::group(['middleware'=>'auth:api'],function(){ //setelah login baru bisa di
     Route::get('users/{id}','Api\AuthController@show');
     Route::put('users/{id}','Api\AuthController@update');
     Route::delete('users/{id}','Api\AuthController@destroy');
+    
+    Route::get('followers/{id}', 'Api\FollowersController@show');
+    Route::get('followers/find/{id}', 'Api\FollowersController@find');
+    Route::delete('followers/{id}', 'Api\FollowersController@destroy');
+    Route::post('followers', 'Api\FollowersController@store');
+    
+    Route::get('followers/{id}', 'Api\FollowersController@show');
+    Route::get('followers/find/{id}', 'Api\FollowersController@find');
+    Route::delete('followers/{id}', 'Api\FollowersController@destroy');
+    Route::post('followers', 'Api\FollowersController@store');
+
+    Route::get('post/{id}', 'Api\PostController@show');
+    Route::delete('post/{id}', 'Api\PostController@destroy');
+    Route::post('post', 'Api\PostController@store');
+    Route::put('post/{id}', 'Api\PostController@update');
 });
