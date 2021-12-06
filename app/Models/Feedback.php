4bcd857a4870ba35930dfa08b7d5f919735cdbf1
EdgarG4m3r <killzone551@ymail.com>
2021-12-06 19:01:39 +0800

    Create Feedback.php

---
+++ app/Models/Feedback.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+
+
+class Feedback extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'feedback_content', 'feedback_star', 'user_id'
+    ];
+
+    public function getCreatedAtAttribute()
+    {
+        if (!is_null($this->attributes['created_at']))
+        {
+            return Carbon::parse($this->attributes['created_at'])->format('Y-m-d H:i:s');
+        }
+    }
+
+    public function getUpdatedAtAttribute()
+    {
+        if (!is_null($this->attributes['updated_at']))
+        {
+            return Carbon::parse($this->attributes['updated_at'])->format('Y-m-d H:i:s');
+        }
+    }
+}
